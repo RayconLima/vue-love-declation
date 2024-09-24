8b862b3e8813f347d42545d9036b24f12457beff
Raycon Lima Batista <rayconlimabatista18@gmail.com>
2024-09-24 00:52:19 -0400

    feat: :sparkles: add images in Home page

--- src/ui/views/Home.vue
+++ src/ui/views/Home.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import Slider from '@/ui/components/Slider.vue'
+import Card from '@/ui/components/Card.vue'
 import { ref } from 'vue';
 
 const isLoaded = ref(false)
@@ -13,22 +14,25 @@ const onImgLoad = () => {
   <Slider duration="duration-500" />
   
   <main>
-    <div class="flex justify-between m-2">
-      <div class="flex items-center">
-        <article>
-          <header>
-            <h1>Uma Jornada Inesquecível</h1> 
-          </header>
-          <main>
-            <span>Descubra uma história de amor que vai te emocionar. </span>
-            <router-link class="px-2 py-1 font-bold text-white rounded hover bg-[--color-secondary] text-[--color-background]" :to="{ name: 'about'}">Começar a Jornada</router-link>
-          </main>
-        </article>
+    <Card>
+      <div class="flex justify-between">
+        <div class="flex items-center">
+          <article>
+            <header>
+              <h1>Uma Jornada Inesquecível</h1> 
+            </header>
+            <main>
+              <span>Descubra uma história de amor que vai te emocionar. </span>
+              <router-link class="px-2 py-1 font-bold text-white rounded hover bg-[--color-secondary] text-[--color-background]" :to="{ name: 'about'}">Começar a Jornada</router-link>
+            </main>
+          </article>
+        </div>
+      
+        <img src="@/ui/assets/images/man_and_heart.png" @load="onImgLoad" class="size-1/3" />
       </div>
-      <img src="@/ui/assets/images/man_and_heart.png" @load="onImgLoad" class="size-1/3" />
-    </div>
+    </Card>
     <div class="relative pt-[56.25%] w-full md:w-full sm:3/5">
-      <iframe class="absolute top-0 left-0 w-full h-full" 
+      <iframe class="absolute top-0 left-0 rounded-lg w-full h-full" 
               src="https://www.youtube.com/embed/O0B54HlcpI0?si=4JwQKz4mjPHg2JXD" 
               title="YouTube video player" 
               frameborder="0" 
