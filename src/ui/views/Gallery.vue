14ff6c1a1bf695e1a392bb3b803a91b97245c58f
Raycon Lima Batista <rayconlimabatista@gmail.com>
2024-09-30 16:02:04 -0400

    feat: :sparkles: implement gallery with new images

--- src/ui/views/Gallery.vue
+++ src/ui/views/Gallery.vue
@@ -1,63 +1,48 @@
 <template>
-    <h1>Galeria</h1>
-
     <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
-        <div class="grid gap-4">
-            <div>
-                <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg" alt="">
-            </div>
-            <div>
-                <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg" alt="">
-            </div>
-            <div>
-                <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg" alt="">
-            </div>
-        </div>
-        <div class="grid gap-4">
-            <div>
-                <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt="">
-            </div>
-            <div>
-                <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg" alt="">
-            </div>
-            <div>
-                <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg" alt="">
-            </div>
-        </div>
-        <div class="grid gap-4">
-            <div>
-                <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg" alt="">
-            </div>
-            <div>
-                <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg" alt="">
-            </div>
-            <div>
-                <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg" alt="">
-            </div>
-        </div>
-        <div class="grid gap-4">
-            <div>
-                <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg" alt="">
-            </div>
-            <div>
-                <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg" alt="">
-            </div>
-            <div>
-                <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg" alt="">
+        <div v-for="(group, index) in imageGroups" :key="index" class="grid gap-4">
+            <div v-for="(image, imgIndex) in group" :key="imgIndex">
+                <img 
+                    class="h-200 max-w-full rounded-lg animate-fade-in-up" 
+                    :src="image.src" :alt="image.alt" :title="image.alt">
             </div>
         </div>
     </div>
 </template>
+
 <script>
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router';
+import images from '@/utils/images.json'; // Importe o arquivo JSON
+
 export default {
+    name: "Gallery",
     setup() {  
         const route = useRoute();
+        const imageGroups = ref([]);
 
         onMounted(() => {
+            console.log(images)
             document.title = route.meta.title || 'Galeria';
-        })
+
+            // Organize as imagens em grupos de 3 para cada coluna
+            const groupedImages = [];
+            const chunkSize     = 3;
+            for (let i = 0; i < images.gallery.length; i += chunkSize) {
+                groupedImages.push(images.gallery.slice(i, i + chunkSize));
+            }
+            imageGroups.value = groupedImages;
+
+            setInterval(() => {
+                images.gallery.forEach((image) => {
+                    image.isVisible = !image.isVisible;
+                });
+            }, 5000);
+        });
+
+        return {
+            imageGroups,
+        };
     }   
 }
 </script>
