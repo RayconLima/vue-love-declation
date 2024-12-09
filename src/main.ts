51e72ea5610f465c3f903845bceb91e1e0c5bf7d
Raycon Lima Batista <rayconlimabatista@gmail.com>
2024-12-09 06:57:47 -0400

    feat: :sparkles: add google analytics

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,16 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import VueGtag from 'vue-gtag-next';
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(VueGtag, {
+    property: {
+      id: import.meta.env.VITE_GA_ID,
+    },
+});
 
 app.mount('#app')
